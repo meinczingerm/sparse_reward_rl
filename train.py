5f343a5a866a377b35b92336e68de5d38f8d9f73
Mark Meinczinger <mark.meinczinger@tum.de>
2022-06-23 16:52:35 +0200

    CHG: update train.py to handle bring near env

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import os
 import warnings
 
+import torch.autograd
 from robosuite import load_controller_config
 from sb3_contrib import TQC
 from sb3_contrib.common.wrappers import TimeFeatureWrapper
@@ -9,15 +10,18 @@ from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.monitor import Monitor
 
 from demonstration.collect import collect_demonstrations
+from demonstration.policies.bring_near.bring_near_policy import BringNearDemonstrationPolicy
 from demonstration.policies.parameterized_reach.policy import ParameterizedReachDemonstrationPolicy
+from env.bring_near import BringNearEnv
 from env.cable_manipulation_base import CableManipulationBase
 from env.parameterized_reach import ParameterizedReachEnv
 from eval import EvalVideoCallback
 from model.hindrl_buffer import HinDRLReplayBuffer, HinDRLTQC, HerReplayBufferWithDemonstrationGoals
 from utils import create_log_dir, save_dict, get_baseline_model_with_name, get_controller_config
 
-config = {
+configs = [{
     "demonstration_hdf5": "/home/mark/tum/2022ss/thesis/master_thesis/demonstration/collection/ParameterizedReach_2Waypoint/1655978351_2683008/demo.hdf5",
+    "demonstration_policy": ParameterizedReachDemonstrationPolicy(),
     "model_config":{
             "policy": "MultiInputPolicy",
             "buffer_size": 1000000,
@@ -31,8 +35,26 @@ config = {
         },
     "env_class": ParameterizedReachEnv,
     "env_kwargs": {"horizon": 200},
-    "demonstration_policy": ParameterizedReachDemonstrationPolicy()
-}
+},
+    {
+    "demonstration_hdf5": "/home/mark/tum/2022ss/thesis/master_thesis/demonstration/collection/BringNear/1655990392_916894/demo.hdf5",
+    "demonstration_policy": BringNearDemonstrationPolicy(),
+    "model_config": {
+        "policy": "MultiInputPolicy",
+        "buffer_size": 1000000,
+        "batch_size": 2048,
+        "gamma": 0.95,
+        "learning_rate": float(1e-3),
+        "tau": 0.05,
+        "verbose": 1,
+        "learning_starts": 200,
+        "policy_kwargs": {"net_arch": [512, 512, 512], "n_critics": 2},
+    },
+    "env_class": BringNearEnv,
+    "env_kwargs": {"horizon": 200,
+                   "use_desired_goal": True},
+
+    }]
 
 
 def _setup_training(demonstration_hdf5, config):
@@ -66,7 +88,11 @@ def train(_config):
     eval_env_config = _config['env_kwargs']
     eval_env_config['has_renderer'] = False
     eval_env_config['has_offscreen_renderer'] = True
+    eval_env_config['use_camera_obs'] = True
     eval_env = Monitor(_config['env_class'](**eval_env_config))
+    eval_env.env.get_desired_goal_fn = env.envs[0].env.get_desired_goal_fn
+    env.reset()
+    eval_env.reset()
     # Use deterministic actions for evaluation
     eval_path = os.path.join(log_dir, 'train_eval')
 
@@ -77,10 +103,12 @@ def train(_config):
                                  log_path=eval_path, eval_freq=1000, n_eval_episodes=10, deterministic=True,
                                  render=False)
     eval_callbacks = CallbackList([video_callback, eval_callback])
+
     model.learn(50000000, callback=eval_callbacks)
 
 
 if __name__ == '__main__':
-    # _collect_demonstration()
+    config = configs[1]
+    # _collect_demonstration(config["env_class"](), config["demonstration_policy"])
     train(config)
     # run_parallel(_configs=)

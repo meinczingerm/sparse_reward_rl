f30f168c35023d020117c25030e8cf0d6358d310
Mark Meinczinger <mark.meinczinger@tum.de>
2022-06-23 12:48:33 +0200

    ADD: uniform demonstration goal sampling

--- model/hindrl_buffer.py
+++ model/hindrl_buffer.py
@@ -21,34 +21,70 @@ class HerReplayBufferWithDemonstrationGoals(HerReplayBuffer):
                                                 np.array(f["data"][demo_id]["engineered_encodings"])})
 
         self.goal_buffer = [demo["engineered_encodings"][-1] for demo in self.demonstrations]
-        for _env in (_env_monitor.env for _env_monitor in env.envs):
-            _env.get_desired_goal_fn = self.get_random_goal
-        super().__init__(env, buffer_size, **kwargs)
-
-    def get_random_goal(self):
-        return copy.deepcopy(self.goal_buffer[random.randint(0, len(self.goal_buffer)-1)])
 
 
 class HinDRLReplayBuffer(HerReplayBuffer):
-    def __init__(self, demonstration_hdf5, env, buffer_size=int(1e5), **kwargs):
+    def __init__(self, demonstration_hdf5, env, replay_strategy="uniform_demonstration",
+                 buffer_size=int(1e5), **kwargs):
         self.demonstration_hdf5 = demonstration_hdf5
-        self.demonstrations = []
+        self.replay_strategy = replay_strategy
+
+        self.demonstrations = {"actions": [],
+                               "observations": []}
+        self.online_goal_buffer = []
 
         for _env in env.envs:
             assert _env.use_engineered_observation_encoding
         with h5py.File(demonstration_hdf5, "r") as f:
             for demo_id in f["data"].keys():
-                self.demonstrations.append({'actions': np.array(f["data"][demo_id]["actions"]),
-                                            'engineered_encodings':
-                                                np.array(f["data"][demo_id]["engineered_encodings"])})
+                actions = np.array(f["data"][demo_id]["actions"])
+                observations = np.array(f["data"][demo_id]["observations"])
+                self.demonstrations["actions"].append(actions)
+                self.demonstrations["observations"].append(observations)
+                self.online_goal_buffer.append(observations[-1])
 
-        self.goal_buffer = [demo["engineered_encodings"][-1] for demo in self.demonstrations]
         for _env in (_env_monitor.env for _env_monitor in env.envs):
-            _env.get_desired_goal_fn = self.get_random_goal
+            _env.get_desired_goal_fn = self.get_online_goal
         super().__init__(env, buffer_size, **kwargs)
 
-    def get_random_goal(self):
-        return copy.deepcopy(self.goal_buffer[random.randint(0, len(self.goal_buffer)-1)])
+    def get_online_goal(self):
+        return copy.deepcopy(self.online_goal_buffer[random.randint(0, len(self.online_goal_buffer) - 1)])
+
+    def sample_goals(self, episode_indices: np.ndarray, her_indices: np.ndarray, transitions_indices: np.ndarray,
+    ) -> np.ndarray:
+        if self.replay_strategy == "uniform_demonstration":
+            return self._sample_uniform_demonstration_goals(num_of_goals=her_indices.shape[0])
+        else:
+            raise NotImplementedError
+
+    def _sample_uniform_demonstration_goals(self, num_of_goals: int):
+        """
+        Get random states uniformly from all demonstration.
+        :param num_of_goals: Number of sampled goals
+        :return: Sampled goals (np.array)
+        """
+        demonstrations = self.demonstrations["observations"]
+        num_of_demonstrations = len(demonstrations)
+        demonstration_indices = np.random.randint(low=0, high=num_of_demonstrations, size=num_of_goals)
+
+        goals = []
+        for i, demo in enumerate(demonstrations):
+            goal_num_from_demo = (demonstration_indices == i).sum()
+            goal_indices = np.random.randint(0, demo.shape[0]-1, goal_num_from_demo)
+            goal_array = np.take(demo, goal_indices, axis=0)
+            goals.append(goal_array)
+
+        goals = np.vstack(goals)
+        goals = goals.reshape([goals.shape[0], 1, goals.shape[1]])
+        return goals
+
+
+
+
+
+
+
+
 
 
 class HinDRLTQC(TQC):
